4c47d14117f93606d2964cc1872205094b907374
Dominik Haslinger <relaxfactor88@gmail.com>
2014-03-26 03:47:20 +0100

    AdMob update.

---
+++ admob/libproj/admob/build/admob.build/Debug-iphonesimulator/admob.build/Objects-normal/i386/GADAdSizeWrapped.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/Haslinger/development/robovm-ios-bindings/admob/libproj/admob/admob/GADAdSizeWrapped.m \
+  /Users/Haslinger/development/robovm-ios-bindings/admob/libproj/admob/admob/GADAdSizeWrapped.h \
+  /Users/Haslinger/development/robovm-ios-bindings/admob/libproj/admob/admob/GADAdSize.h
---
+++ admob/src/org/robovm/bindings/admob/GADAdSize.java
@@ -0,0 +1,39 @@
+
+package org.robovm.bindings.admob;
+
+import org.robovm.apple.coregraphics.CGSize;
+import org.robovm.rt.bro.Struct;
+import org.robovm.rt.bro.annotation.ByVal;
+import org.robovm.rt.bro.annotation.StructMember;
+
+public class GADAdSize extends Struct<GADAdSize> {
+	private GADAdSize () {
+	}
+
+	@StructMember(0)
+	public native @ByVal
+	CGSize size ();
+
+	@StructMember(0)
+	public native GADAdSize size (@ByVal CGSize size);
+
+	@StructMember(1)
+	public native int flags ();
+
+	@StructMember(1)
+	public native GADAdSize flags (int flags);
+
+	public boolean isValid () {
+		return GADAdSizeManager.isValid(this);
+	}
+
+	@Override
+	public boolean equals (Object obj) {
+		return obj instanceof GADAdSize && GADAdSizeManager.equals(this, (GADAdSize)obj);
+	}
+
+	@Override
+	public String toString () {
+		return GADAdSizeManager.toString(this);
+	}
+}
