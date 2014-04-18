db37cc5abd3a95c5951b663d2b7b667123499793
Dominik Haslinger <relaxfactor88@gmail.com>
2014-04-18 02:09:58 +0200

    Fix for Admob GADRequestError bug

--- admob/src/org/robovm/bindings/admob/GADRequestError.java
+++ admob/src/org/robovm/bindings/admob/GADRequestError.java
@@ -4,8 +4,10 @@ package org.robovm.bindings.admob;
 import org.robovm.apple.foundation.NSError;
 import org.robovm.objc.annotation.NativeClass;
 import org.robovm.rt.bro.annotation.GlobalValue;
+import org.robovm.rt.bro.annotation.Library;
 
 /** This class represents the error generated due to invalid request parameters. */
+@Library(Library.INTERNAL)
 @NativeClass
 public class GADRequestError extends NSError {
 	/** Google AdMob Ads error domain. */
