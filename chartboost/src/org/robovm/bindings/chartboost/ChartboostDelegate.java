589076d6fa4cc2ac5bd1978a4669df9af6c022bd
oscarbigu <oscarbigu@gmail.com>
2014-10-10 19:58:37 +0200

    Updated Chartboost bindings to 5.0

--- chartboost/src/org/robovm/bindings/chartboost/ChartboostDelegate.java
+++ chartboost/src/org/robovm/bindings/chartboost/ChartboostDelegate.java
@@ -1,52 +1,53 @@
-
 package org.robovm.bindings.chartboost;
 
 import org.robovm.apple.foundation.NSObjectProtocol;
 import org.robovm.objc.annotation.Method;
 
 public interface ChartboostDelegate extends NSObjectProtocol {
+
 	@Method(selector = "shouldRequestInterstitial:")
-	boolean shouldRequestInterstitial (String location);
+	boolean shouldRequestInterstitial(String location);
 
 	@Method(selector = "shouldDisplayInterstitial:")
-	boolean shouldDisplayInterstitial (String location);
+	boolean shouldDisplayInterstitial(String location);
+
+	@Method(selector = "didDisplayInterstitial:")
+	void didDisplayInterstitial(String location);
 
 	@Method(selector = "didCacheInterstitial:")
-	void didCacheInterstitial (String location);
+	void didCacheInterstitial(String location);
 
-	@Method(selector = "didFailToLoadInterstitial:")
-	void didFailToLoadInterstitial (String location);
+	@Method(selector = "didFailToLoadInterstitial:withError:")
+	void didFailToLoadInterstitial(String location, CBLoadError loadError);
+
+	@Method(selector = "didFailToRecordClick:withError:")
+	void didFailToRecordClick(String location, CBClickError clickError);
 
 	@Method(selector = "didDismissInterstitial:")
-	void didDismissInterstitial (String location);
+	void didDismissInterstitial(String location);
 
 	@Method(selector = "didCloseInterstitial:")
-	void didCloseInterstitial (String location);
+	void didCloseInterstitial(String location);
 
 	@Method(selector = "didClickInterstitial:")
-	void didClickInterstitial (String location);
+	void didClickInterstitial(String location);
 
-	@Method(selector = "shouldDisplayLoadingViewForMoreApps")
-	boolean shouldDisplayLoadingViewForMoreApps ();
+	@Method(selector = "shouldDisplayMoreApps:")
+	boolean shouldDisplayMoreApps(String location);
 
-	@Method(selector = "shouldDisplayMoreApps")
-	boolean shouldDisplayMoreApps ();
+	@Method(selector = "didCacheMoreApps:")
+	void didCacheMoreApps(String location);
 
-	@Method(selector = "didCacheMoreApps")
-	void didCacheMoreApps ();
+	@Method(selector = "didDismissMoreApps:")
+	void didDismissMoreApps(String location);
 
-	@Method(selector = "didFailToLoadMoreApps")
-	void didFailToLoadMoreApps ();
+	@Method(selector = "didCloseMoreApps:")
+	void didCloseMoreApps(String location);
 
-	@Method(selector = "didDismissMoreApps")
-	void didDismissMoreApps ();
+	@Method(selector = "didClickMoreApps:")
+	void didClickMoreApps(String location);
 
-	@Method(selector = "didCloseMoreApps")
-	void didCloseMoreApps ();
+	@Method(selector = "didFailToLoadMoreApps:withError:")
+	void didFailToLoadMoreApps(String location, CBLoadError loadError);
 
-	@Method(selector = "didClickMoreApps")
-	void didClickMoreApps ();
-
-	@Method(selector = "shouldRequestInterstitialsInFirstSession")
-	boolean shouldRequestInterstitialsInFirstSession ();
 }
