c81f69683ed29ea688392a534678724832ba029e
BlueRiverTeam <blueriverteam@gmail.com>
2014-12-10 00:28:04 +0100

    Recreate and update the admob library.

--- admob/libproj/admob/build/admob.build/Debug-iphonesimulator/admob.build/Objects-normal/i386/GADRequestErrorWrapped.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/Haslinger/development/robovm-ios-bindings/admob/libproj/admob/admob/GADRequestErrorWrapped.m \
-  /Users/Haslinger/development/robovm-ios-bindings/admob/libproj/admob/admob/GADRequestErrorWrapped.h \
-  /Users/Haslinger/development/robovm-ios-bindings/admob/libproj/admob/admob/GADRequestError.h
