feb4cf3ab8af443604842509892c2e8672c39bc3
BlueRiverTeam <blueriverteam@gmail.com>
2015-01-02 01:32:13 +0100

    Updated FBLinkShareParams and FBGraphPlace to allow specification of a place in the share dialog (fixes #124).

--- facebook/src/org/robovm/bindings/facebook/dialogs/FBLinkShareParams.java
+++ facebook/src/org/robovm/bindings/facebook/dialogs/FBLinkShareParams.java
@@ -1,7 +1,10 @@
 
 package org.robovm.bindings.facebook.dialogs;
 
+import org.robovm.apple.foundation.NSObject;
+import org.robovm.apple.foundation.NSString;
 import org.robovm.apple.foundation.NSURL;
+import org.robovm.bindings.facebook.FBGraphPlace;
 import org.robovm.objc.annotation.Method;
 import org.robovm.objc.annotation.NativeClass;
 import org.robovm.objc.annotation.Property;
@@ -12,42 +15,36 @@ import org.robovm.rt.bro.annotation.Pointer;
 @NativeClass
 public class FBLinkShareParams extends FBDialogsParams {
 
-    /** The URL link to be attached to the post. Only "http" or "https" schemes are supported. */
+    /** @return The URL link to be attached to the post. Only "http" or "https" schemes are supported. */
     @Property
     public native NSURL getLink ();
 
     @Property
     public native void setLink (NSURL link);
 
-    /** abstract The name, or title associated with the link. Is only used if the link is not null. */
+    /** @return The name, or title associated with the link. Is only used if the link is not null. */
     @Property
     public native String getName ();
 
     @Property
     public native void setName (String name);
 
-    /*
-     * ! @abstract The caption to be used with the link. Is only used if the link is non-nil.
-     */
+    /** @return The caption to be used with the link. Is only used if the link is non-nil. */
     @Property
     public native String getCaption ();
 
     @Property
     public native void setCaption (String caption);
 
-    /*
-     * ! @abstract The description associated with the link. Is only used if the link is non-nil.
-     */
+    /** @return The description associated with the link. Is only used if the link is non-nil. */
     @Property
     public native String getLinkDescription ();
 
     @Property
     public native void setLinkDescription (String linkDescription);
 
-    /*
-     * ! @abstract The link to a thumbnail to associate with the post. Is only used if the link is non-nil. Only "http" or "https"
-     * schemes are supported. Note that this property should not be used to share photos; see the photos property.
-     */
+    /** @return The link to a thumbnail to associate with the post. Is only used if the link is non-nil. Only "http" or "https"
+     *         schemes are supported. Note that this property should not be used to share photos; see the photos property. */
     @Property
     public native NSURL getPicture ();
 
@@ -58,13 +55,37 @@ public class FBLinkShareParams extends FBDialogsParams {
 // If using NSStrings, the values must represent the IDs of the users to tag. */
 // @property (nonatomic, copy) NSArray *friends;
 
-// /*! @abstract An NSString or FBGraphPlace to tag in the status update. If
-// NSString, the value must be the ID of the place to tag. */
-// @property (nonatomic, copy) id place;
+    /** @return a String to tag in the status update. */
+    public String getPlaceID () {
+        NSString val = (NSString)getPlace0();
+        return val.toString();
+    }
 
-// /*! @abstract A text reference for the category of the post, used on Facebook
-// Insights. */
-// @property (nonatomic, copy) NSString *ref;
+    /** @return a FBGraphPlace to tag in the status update. */
+    public FBGraphPlace getPlace () {
+        return getPlace();
+    }
+
+    @Property(selector = "place")
+    private native NSObject getPlace0 ();
+
+    public void setPlaceID (String placeID) {
+        setPlace(new NSString(placeID));
+    }
+
+    public void setPlace (FBGraphPlace place) {
+        setPlace(place);
+    }
+
+    @Property(selector = "setPlace:")
+    private native void setPlace (NSObject place);
+
+    /** @return A text reference for the category of the post, used on Facebook Insights. */
+    @Property
+    public native String getRef ();
+
+    @Property
+    public native void setRef (String ref);
 
 // /*! @abstract If YES, treats any data failures (e.g. failures when getting
 // data for IDs passed through "friends" or "place") as a fatal error, and will not
